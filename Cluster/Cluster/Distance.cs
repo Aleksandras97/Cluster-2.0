d7ec1b3d886dd1fb904e9f3c379b22cf853c2fc4
agnmol <43700947+agnmol@users.noreply.github.com>
2018-12-01 17:10:15 +0200

    Add files via upload

---
+++ Cluster/Cluster/Distance.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cluster
+{
+    class Distance
+    {
+        public string Key { get; set; }
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public double Dis { get; set; }
+        public double Time { get; set; }
+
+        public Distance(string origin, string destination, double distance, double time)
+        {
+            Origin = origin;
+            Destination = destination;
+            Dis = distance;
+            Time = time;
+            Key = string.Concat(origin, '-', destination);
+        }
+
+        public Distance(string key, string pradzia, string pabaiga, double kelias, double laikas)
+        {
+            Key = key;
+            Origin = pradzia;
+            Destination = pabaiga;
+            Dis = kelias;
+            Time = laikas;
+        }
+
+        public Distance(string line)
+        {
+            string[] vals = line.Split(',');
+            Key = vals[0];
+            Origin = vals[1];
+            Destination = vals[2];
+            Dis = Double.Parse(vals[3]);
+            Time = Double.Parse(vals[4]);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1} {2} {3} {4}", Key, Origin, Destination, Dis, Time);
+        }
+    }
+}
--- Cluster/Cluster/Program.cs
+++ Cluster/Cluster/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,23 +11,25 @@ namespace Cluster
     {
         const double Pastovūs_sandėlio_statybos_kaštai = 308525.05;
         const double Kintantys_sandėlio_statybos_kaštai = 539.91;
-        const double Postovūs_sandėlio_valdymo_kaštai = 8513.26;
+        const double Pastovūs_sandėlio_valdymo_kaštai = 8513.26;
         const double Kintantys_sandėlio_valdymo_kaštai = 6.31;
         const double Sunkvežimio_pristatymo_kaštai = 125.41;
         const double Geležinkelio_pristatymo_kaštai = 3.95;
         const double Sunkvežimio_emisijos_lygis = 0.062;
         const double Traukinio_emisijos_lygis = 0.022;
-        const string DistancesPath = @"";
-        const string Flows = @"";
-        const List<string> Warehouses;
+        const string DistancesPath = @"Distances NUTS2-NUTS2.csv";
+        const string FlowsPath = @"All flows";
 
         static void Main(string[] args)
         {
+            List<Distance> distances = ReadExcelDistances();
+            List<Flow> flows = ReadExcelFlows();
+            Console.WriteLine(CountPriceWithoutWarehouses(distances, flows));
         }
 
-        private static void ReadExcelDistances(List<City> cities)
+        private static List<Distance> ReadExcelDistances()
         {
-            List<string> cityNames = new List<string>();
+            List<Distance> distances = new List<Distance>();
             using (var reader = new StreamReader(DistancesPath))
             {
                 var line = reader.ReadLine();
@@ -35,33 +38,43 @@ namespace Cluster
                 while (!reader.EndOfStream)
                 {
                     line = reader.ReadLine();
-                    values = line.Split(';');
-                    City city = new City(values[0]...);
-                    cities.Add(city);
-                    cityNames.Add(values[0]);
-                }
-                cityNames = cityNames.Distinct();
-                ()
-                {
-                    if (x.name)
+                    Distance distance = new Distance(line);
+                    if (distance.Origin.CompareTo(distance.Destination) != 0)
+                    {
+                        distances.Add(distance);
+                    }
                 }
             }
+            return distances;
         }
-        private static void ReadExcelFlows()
+        private static List<Flow> ReadExcelFlows()
         {
-            using (var reader = new StreamReader(Flows))
+            List<Flow> flows = new List<Flow>();
+            using (var reader = new StreamReader(DistancesPath))
             {
-                List<string> listA = new List<string>();
-                List<string> listB = new List<string>();
+                var line = reader.ReadLine();
+                var values = line.Split(';');
+
                 while (!reader.EndOfStream)
                 {
-                    var line = reader.ReadLine();
-                    var values = line.Split(';');
-
-                    listA.Add(values[0]);
-                    listB.Add(values[1]);
+                    line = reader.ReadLine();
+                    Flow flow = new Flow(line);
+                    if(flow.Load.CompareTo(flow.Unload) != 0)
+                    {
+                        flows.Add(flow);
+                    }
                 }
             }
+            return flows;
+        }
+        public static double CountPriceWithoutWarehouses(List<Distance> distances, List<Flow> flows)
+        {
+            double price = flows.Sum(x => x.FlowTons) * distances.Sum(x=> x.Dis) * Sunkvežimio_pristatymo_kaštai;
+            double polution = flows.Sum(x => x.FlowTons) * distances.Sum(x => x.Dis) * Sunkvežimio_emisijos_lygis;
+
+            double total = price + polution;
+
+            return total;
         }
 
     }
